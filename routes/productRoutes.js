3d39ffa41b6c41abe013c4c22e2a8bcdc73a8fcb
Nino-Prog <160301788+Nino-Prog@users.noreply.github.com>
2024-04-18 02:07:58 -0400

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,9 @@
 const products = [
-    { id: 1, name: "Eagle & Letter Print Quarter Zip Sweatshirt", description: "Comfortable and stylish.", category: "Clothing", image_path: "images/product1.jpg", price: 50.00 },
-    { id: 2, name: "Men's Stylish Loose Alphabets Pattern Knitted Sweater", description: "Ideal for winter.", category: "Clothing", image_path: "images/product2.jpg", price: 40.00 },
-    { id: 3, name: "Trendy Goose Pattern Knitted Sweater", description: "Perfect for a casual day out.", category: "Clothing", image_path: "images/product3.jpg", price: 45.00 },
-    { id: 4, name: "Painting Pattern Knitted Sweater", description: "Artistic and cozy.", category: "Clothing", image_path: "images/product4.jpg", price: 55.00 },
-    { id: 5, name: "Retro Knitted Color Block Sweater", description: "Retro vibes with modern fit.", category: "Clothing", image_path: "images/product5.jpg", price: 60.00 }
+    { id: 1, name: "Eagle & Letter Print Quarter Zip Sweatshirt", description: "Comfortable and stylish.", category: "Clothing", image_path: "images/Eagle-_-Letter-Print-Quarter-Zip-Sweatshirt.jpg", price: 50.00 },
+    { id: 2, name: "Men's Stylish Loose Alphabets Pattern Knitted Sweater", description: "Ideal for winter.", category: "Clothing", image_path: "Images/Men_s-Stylish-Loose-Alphabets-Pattern-Knitted-Sweater.jpg", price: 40.00 },
+    { id: 3, name: "Trendy Goose Pattern Knitted Sweater", description: "Perfect for a casual day out.", category: "Clothing", image_path: "images/Men_s-Trendy-Goose-Pattern-Knitted-Sweater.jpg", price: 45.00 },
+    { id: 4, name: "Painting Pattern Knitted Sweater", description: "Artistic and cozy.", category: "Clothing", image_path: "images/Painting-Pattern-Knitted-Sweater.jpg", price: 55.00 },
+    { id: 5, name: "Retro Knitted Color Block Sweater", description: "Retro vibes with modern fit.", category: "Clothing", image_path: "images/Retro-Knitted-Color-Block-Sweater.jpg", price: 60.00 }
 ];
 
 // Function to get all products
